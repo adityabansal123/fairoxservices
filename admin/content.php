05c44a888e7376d9cc769b60bc205b8cbb224f2a
Aditya Bansal <eadityabansal@gmail.com>
2019-02-08 00:59:24 +0530

    view implementation

--- admin/content.php
+++ admin/content.php
@@ -1,5 +1,8 @@
 <!-- Header File (navebar) included. -->
-<?php include('header.php'); ?>
+<?php 
+include('header.php'); 
+include('config/config.php');
+?>
 
   
 <div id="container">
@@ -7,17 +10,36 @@
             <div class="alert alert-danger" role="alert">
                 <strong>Invalid ID! </strong> No Record Found.
             </div>
-        
-        <!-- This DIV will Display TinyMCE Content. -->
-		<div class="card"  style="color: black;">
-            <div class="card-header">
-                Title
-            </div>
-            <div class="card-body">
-                <p class="card-text">Content is here.</p>
-                <a href="showcontent.php" class="btn btn-primary">Back</a>
-            </div>
-        </div>
+
+            <?php
+                if(isset($_GET['id'])){
+                    $id = $_GET['id'];
+                    $sql = 'SELECT * FROM content WHERE id='.$id;
+
+                    $result = mysqli_query($con, $sql);
+                    if(mysqli_num_rows($result)>0){
+                        while($row = mysqli_fetch_assoc($result)){
+                            echo '<div class="card"  style="color: black;">
+                                <div class="card-header">
+                                    '. $row["title"] .'
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">'.$row["notes"].'</p>
+                                    <a href="showcontent.php" class="btn btn-primary">Back</a>
+                                </div>
+                            </div>';
+                        }
+                    }else{
+                        echo '
+                            <div class="alert alert-danger" role="alert">
+                                No Record Found
+                            </div>
+                        ';
+                    }
+                }else{
+                    header('Location: showcontent.php');
+                }
+            ?>
 
 </div>
 
