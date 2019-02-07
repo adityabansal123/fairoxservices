537cf3f5040ad6878fd42bdcfd6812f334e72e48
Aditya Bansal <eadityabansal@gmail.com>
2019-02-08 00:19:52 +0530

    deletion

--- admin/showcontent.php
+++ admin/showcontent.php
@@ -7,22 +7,35 @@ include('config/config.php');
 
   
 <div id="container">
-<!-- Form for Delete confirmation --> 
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Are you delete this record?</strong>
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-  <form target="" method="post">
-     <br>
-      <input type="hidden" name="delId" value="">
-      <button type="submit" name="delete" class="btn btn-danger">Yes</button>
-      
-      <button type="button" class="btn btn-info" data-dismiss="alert">
-    <span aria-hidden="true">Oops! No </span>
-  </button>      
-  </form>
-</div>
+
+  <?php
+    if(isset($_GET['deleteID'])){
+      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Are you delete this record?</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <form target="" method="post">
+          <br>
+            <input type="hidden" name="delId" value="'.$_GET["deleteID"].'">
+            <button type="submit" name="delete" class="btn btn-danger">Yes</button>
+            
+            <button type="button" class="btn btn-info" data-dismiss="alert">
+              <span aria-hidden="true">Oops! No </span>
+            </button>      
+        </form>
+      </div>';
+    }
+    if(isset($_POST['delID'])){
+      $id = $_POST['delID'];
+      $sql = "DELETE FROM content WHERE id=".$id;
+      if(mysqli_query($con, $sql)){
+        header("Location: showcontent.php");
+      }else{
+        echo "Error! Record not deleted".mysqli_error($con);
+      }
+    }
+?>
     
 <!-- Success Message if Record Deleted OR Uptated successfully. --> 
 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -55,9 +68,9 @@ include('config/config.php');
                       <td>'.$row["id"].'</td>
                       <td>'.$row["title"].'</td>
                       <td>'.$row["addedDate"].'</td>
-                      <td><a href="#" class="btn btn-success"><i class="fas fa-eye"></i> </a></td>
-                      <td><a href="#" class="btn btn-primary"><i class="fas fa-edit"></i> </a></td>
-                      <td><a href="#" class="btn btn-danger"><i class="fas fa-trash-alt"></i> </a></td>
+                      <td><a href="content.php?id='. $row["id"] .'" class="btn btn-success"><i class="fas fa-eye"></i> </a></td>
+                      <td><a href="upadte.php?updateID='. $row["id"] .'" class="btn btn-primary"><i class="fas fa-edit"></i> </a></td>
+                      <td><a href="showcontent.php?deleteID='. $row["id"] .'" class="btn btn-danger"><i class="fas fa-trash-alt"></i> </a></td>
                   </tr>';
                 }
               }else{
